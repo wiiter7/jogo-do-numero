482864664976c55614f7a4ddf6aa86406c8152cc
wiiter7 <arthur.carneiro.santos@escola.pr.gov.br>
2025-08-14 07:43:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-
-const  palpites = document.querySelector('.palpites');
-const ultimoResultado = document.querySelector('.umtimoResultado');
-const baixoOuAuto = document.querySelector('.baixoOuAlto');
+let numeroAleatorio = Math.florr(Math.random() *100) +1;
+const palpites = document.querySelector('.palpites');
+const ultimoResultado = document.querySelector('.ultimoResultado');
+const baixoOuAlto = document.querySelector('.baixoOuAlto');
 const envioPalpite = document.querySelector('.envioPalpite');
 const campoPalpite = document.querySelector('.campoPalpite');
+let contagemPalpites = 1;
+let botaoReiniciar;
 
-
-
+                                            
 function verificarPalpite() {
-    const palpiteUsuario = Number(campoPalpite.value);
-    if (contagemPalpites === 1) {
-        palpites.textContent = "Palpites anteriores: ";
-    }
+  const palpiteUsuario = Number(campoPalpite.value);
+  if (contagemPalpites === 1) {
+    palpites.textContent = "Palpites anteriores: ";
+  }
 
-    palpites.textContent += palpiteUsuario + " ";
+palpites.textContent += palpiteUsuario + " ";
 
-    if (palpiteUsuario === numeroAleatorio) {
-        ultimoResultado.textContent = "Parabens ! Você Acertou";
-        
+if (palpiteUsuario === numeroAleatorio) {
+  ultimoResultado.textContent = "Parabéns! Você Acertou!";
+  ultimoResultado.style.backgroundColor = "green";
+  baixoOuAlto.textContent = "";
+  finalizarJogo();
+} else if (contagemPalpites === 10 {
+    ultimoResultado.textcontent = "FIM DE JOGO";
+    baixoOuAuto.textcontent = "";
+    finalizarJogo();
+} else {
+    ultimoResultado.textContent= "errado";
+    ultimoResultado.style.backgroundColor = "red";
+    if ( palpiteUsuario ,numeroAleatorio) {
+        baixoOuAlto.textXontent ="O último palpite foi muito baixo";
+    } else if(palpiteUsuario . numeroAleatorio) {
+        baixoAuto.textContent = "o último palpite foi muito alto";
     }
 }
